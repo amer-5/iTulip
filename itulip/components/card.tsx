11a9ad344577420bd9911c416f6b2ce7c2ad5148
Amer <amerlopo5@gmail.com>
2025-05-03 22:29:26 +0200

    Cards

--- itulip/components/card.tsx
+++ itulip/components/card.tsx
@@ -1,37 +1,61 @@
 import React from "react";
+import Image from "next/image";
 
-interface CardProps {
+import Button from "@/components/button.tsx";
+
+import Style from "@/styles/components/card.module.css";
+
+interface BaseProps {
   heading: string;
   icon: string;
-  text?: string;
-  buttonText?: string;
-  features?: string[];
 }
 
+interface TableProps extends BaseProps {
+  type: "table";
+  text: string;
+}
+
+interface ContactProps extends BaseProps {
+  type: "contact";
+  buttonText: string;
+  features: string[];
+}
+
+type CardProps = TableProps | ContactProps;
+
 const Card: React.FC<CardProps> & {
-  Table: React.FC<CardProps>;
-  Contact: React.FC<CardProps>;
-} = ({ heading, icon, text, buttonText, features }) => {
+  Table: React.FC<Omit<TableProps, "type">>;
+  Contact: React.FC<Omit<ContactProps, "type">>;
+} = (props) => {
+  const { heading, icon } = props;
+
   return (
-    <>
-      <div>{icon + heading}</div>
-      {text && <p>{text}</p>}
-      {buttonText && <button>{buttonText}</button>}
-      {features && (
-        <ul>
-          {features.map((f, i) => (
-            <li key={i}>{f}</li>
-          ))}
-        </ul>
+    <div className={Style.parent}>
+      <div className={Style.header}>
+        <Image src={icon} alt="Icon" width={24} height={24} />
+        <p className={Style.heading}>{heading}</p>
+      </div>
+
+      {props.type === "table" && <p className={Style.text}>{props.text}</p>}
+
+      {props.type === "contact" && (
+        <>
+          <Button>Contact us now</Button>
+          <ul className={Style.list}>
+            {props.features.map((feature, i) => (
+              <li key={i}>{feature}</li>
+            ))}
+          </ul>
+        </>
       )}
-    </>
+    </div>
   );
 };
 
-Card.Table = (props) => <Card {...props} />;
+Card.Table = (props) => <Card {...props} type="table" />;
 Card.Table.displayName = "Card.Table";
 
-Card.Contact = (props) => <Card {...props} />;
+Card.Contact = (props) => <Card {...props} type="contact" />;
 Card.Contact.displayName = "Card.Contact";
 
 export default Card;
