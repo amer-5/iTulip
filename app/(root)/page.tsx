558401b4cfa432d76c0052ec4ee40faa9c97a9b8
AmarPasalic <apasalic087@gmail.com>
2025-05-15 10:41:59 +0200

    zavrsen responsive cluster selcije

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,5 @@
-import Card from "@/components/card";
+
 import Table from "@/sections/table";
-import Icon from "@/public/icons/falling-star.svg";
 import Rows from "@/sections/rows";
 import Style from "@/styles/pages/page.module.css";
 import SmallTable from "@/sections/smallTable";
@@ -14,17 +13,6 @@ export default function Home() {
     <div className={Style.parent}>
       <Map />
       <Table />
-      <Card.Contact
-        heading="CONTINUITEIT"
-        icon={Icon}
-        buttonText="Contact us now"
-        features={[
-          "Merciere teams beschikbaar voor langdurige projecten",
-          "Ontwikkelaars met jarenlange ervaring eerst/meerwerk",
-          "Werkt volgens Agile methode",
-          "Flexibel en schaalbaar",
-        ]}
-      />
       <Rows />
       <ClusterPage />
       <PageTxt />
